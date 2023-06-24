43e4638d88212bda3ab27c3693db0376e6b1fa53
Rawan <rawnq.sb@gmail.com>
2023-06-25 01:04:17 +0300

    add skills

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
 
     <>
   
-     <div id="conteneur-footer">
+     {/* <div id="conteneur-footer">
         <div class="footer">
         
              <span><a href="https://github.com/rrawann" target="_blank"><i class="fa fa-github fa-2x"></i></a></span>
       <span><a href="" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a></span>
         </div>
-      </div>
+      </div> */}
 
 
     </>
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -4,101 +4,93 @@ const Skiils = () => {
 
   return (
     <>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-  <div class="bg-white rounded-lg shadow-lg p-6 transform hover:scale-105 transition duration-300">
-    <div class="text-center">
-      <img src="https://media4.giphy.com/media/hpXdHPfFI5wTABdDx9/giphy.gif?cid=ecf05e47ktbynzqdbinfa2jtzmat77nqz1g713cvfzsazywf&ep=v1_gifs_related&rid=giphy.gif&ct=g" alt="HTML/CSS" class="w-16 h-16 mx-auto mb-4"/>
-      <h3 class="text-xl font-semibold">HTML/CSS</h3>
-    </div>
-    <ul class="mt-4">
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        Responsive Design
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        CSS Flexbox
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        CSS Grid
-      </li>
-    </ul>
-  </div>
+   
+   <div class="container">
   
-  <div class="bg-white rounded-lg shadow-lg p-6 transform hover:scale-105 transition duration-300">
-    <div class="text-center">
-      <img src="https://media4.giphy.com/media/hpXdHPfFI5wTABdDx9/giphy.gif?cid=ecf05e47ktbynzqdbinfa2jtzmat77nqz1g713cvfzsazywf&ep=v1_gifs_related&rid=giphy.gif&ct=g" alt="JavaScript" class="w-16 h-16 mx-auto mb-4"/>
-      <h3 class="text-xl font-semibold">JavaScript</h3>
-    </div>
-    <ul class="mt-4">
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        ES6 Syntax
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        DOM Manipulation
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        AJAX
-      </li>
-    </ul>
-  </div>
+   <svg class="logo-react" width="150" height="236" viewBox="0 0 236 236" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+  
+<circle cx="118" cy="114" r="111.5" stroke="#20232A" stroke-width="5"/>
+<circle cx="118" cy="114" r="90.5" fill="#20232A" stroke="#20232A"/>
+<path class="path-react" d="M130.601 113.804C130.601 117.287 129.218 120.627 126.756 123.091C124.293 125.554 120.954 126.939 117.471 126.94C113.987 126.94 110.645 125.556 108.182 123.093C105.718 120.629 104.334 117.288 104.334 113.804C104.334 110.321 105.717 106.98 108.18 104.517C110.642 102.054 113.982 100.669 117.465 100.667C120.949 100.667 124.29 102.051 126.754 104.515C129.217 106.978 130.601 110.32 130.601 113.804V113.804ZM146.158 51C138.251 51 127.905 56.64 117.441 66.4043C106.984 56.6929 96.632 51.1175 88.7301 51.1175C86.3214 51.1175 84.13 51.6639 82.2324 52.7507C74.1543 57.4096 72.3448 71.9267 76.516 90.1451C58.6325 95.6676 47 104.498 47 113.804C47 123.145 58.6913 131.999 76.6276 137.48C72.4916 155.769 74.3364 170.309 82.4321 174.962C84.3121 176.061 86.4859 176.578 88.9064 176.578C96.8082 176.578 107.16 170.938 117.623 161.162C128.081 170.879 138.433 176.455 146.335 176.455C148.743 176.455 150.935 175.926 152.832 174.839C160.904 170.186 162.72 155.669 158.549 137.445C176.367 131.969 188 123.121 188 113.804C188 104.462 176.309 95.6089 158.372 90.1157C162.508 71.8445 160.664 57.2921 152.568 52.6332C150.7 51.5522 148.526 51.0059 146.152 51H146.158ZM146.129 57.4037V57.439C147.451 57.439 148.514 57.6975 149.407 58.1851C153.32 60.4294 155.018 68.9657 153.696 79.9461C153.379 82.6486 152.862 85.498 152.227 88.4061C146.587 87.0196 140.442 85.9562 133.973 85.2689C130.096 79.952 126.072 75.1227 122.018 70.8927C131.371 62.1977 140.154 57.4272 146.135 57.4096L146.129 57.4037ZM88.7301 57.5212C94.6756 57.5212 103.5 62.2682 112.876 70.9162C108.846 75.1462 104.828 79.9461 101.009 85.263C94.5052 85.9504 88.3541 87.0137 82.72 88.4237C82.062 85.545 81.5744 82.7602 81.2278 80.0812C79.8765 69.1067 81.545 60.5762 85.4225 58.3026C86.5387 57.7739 87.7725 57.5565 88.7301 57.5271V57.5212ZM117.412 75.44C120.085 78.1895 122.758 81.268 125.402 84.6285C122.817 84.511 120.173 84.4287 117.5 84.4287C114.797 84.4287 112.124 84.4875 109.51 84.6285C112.095 81.268 114.768 78.1895 117.412 75.4341V75.44ZM117.5 90.8677C121.848 90.8677 126.177 91.0675 130.437 91.4141C132.822 94.8334 135.149 98.4817 137.387 102.342C139.572 106.102 141.558 109.92 143.368 113.774C141.558 117.622 139.572 121.471 137.416 125.231C135.184 129.108 132.875 132.798 130.484 136.217C126.207 136.587 121.871 136.793 117.5 136.793C113.152 136.793 108.823 136.587 104.563 136.246C102.178 132.827 99.8515 129.173 97.6131 125.319C95.4276 121.559 93.4419 117.74 91.6324 113.886C93.4125 110.026 95.4276 106.172 97.5837 102.406C99.8162 98.5287 102.125 94.851 104.516 91.4317C108.793 91.0557 113.129 90.856 117.5 90.856V90.8677ZM96.1444 92.36C94.7344 94.5749 93.3244 96.8426 92.0084 99.175C90.6865 101.466 89.4528 103.769 88.2778 106.072C86.7209 102.218 85.399 98.376 84.3062 94.6336C88.0662 93.7524 92.0319 92.971 96.1444 92.3659V92.36ZM138.797 92.36C142.88 92.9651 146.816 93.7112 150.582 94.6336C149.525 98.3466 148.203 102.165 146.705 105.99C145.53 103.699 144.296 101.39 142.945 99.0927C141.623 96.7897 140.213 94.5455 138.803 92.36H138.797ZM156.792 96.3256C159.635 97.2069 162.338 98.188 164.87 99.2514C175.046 103.599 181.626 109.286 181.626 113.798C181.596 118.31 175.016 124.02 164.841 128.338C162.373 129.396 159.671 130.348 156.88 131.235C155.235 125.607 153.085 119.743 150.418 113.727C153.061 107.752 155.176 101.919 156.792 96.3139V96.3256ZM78.0964 96.3491C79.7296 101.989 81.8858 107.846 84.5589 113.857C81.9151 119.831 79.7884 125.665 78.1786 131.27C75.3351 130.389 72.6326 129.402 70.1299 128.333C59.9544 124.003 53.3744 118.31 53.3744 113.798C53.3744 109.286 59.9544 103.564 70.1299 99.2514C72.5974 98.1939 75.2999 97.2421 78.0964 96.3491V96.3491ZM146.705 121.494C148.262 125.354 149.583 129.202 150.676 132.939C146.916 133.861 142.945 134.642 138.832 135.23C140.242 133.027 141.652 130.753 142.974 128.427C144.296 126.135 145.53 123.797 146.711 121.494H146.705ZM88.2778 121.612C89.4528 123.915 90.6865 126.212 92.0377 128.515C93.389 130.806 94.7696 133.05 96.1796 135.23C92.0965 134.631 88.1602 133.879 84.3944 132.962C85.4519 129.261 86.7796 125.43 88.2719 121.606L88.2778 121.612ZM152.28 139.16C152.938 142.057 153.455 144.847 153.772 147.52C155.123 158.495 153.455 167.025 149.577 169.305C148.714 169.834 147.592 170.057 146.27 170.057C140.324 170.057 131.5 165.316 122.124 156.662C126.154 152.432 130.172 147.638 133.991 142.327C140.495 141.634 146.646 140.564 152.28 139.154V139.16ZM82.7788 139.219C88.4188 140.594 94.564 141.657 101.032 142.344C104.91 147.661 108.934 152.491 112.988 156.715C103.617 165.427 94.8225 170.198 88.8418 170.198C87.5493 170.168 86.4565 169.904 85.5929 169.422C81.6801 167.19 79.9823 158.648 81.3041 147.667C81.6214 144.965 82.1384 142.121 82.7729 139.219H82.7788ZM109.569 142.979C112.154 143.096 114.797 143.179 117.471 143.179C120.173 143.179 122.846 143.12 125.461 142.979C122.876 146.339 120.203 149.412 117.559 152.173C114.886 149.412 112.212 146.339 109.569 142.979Z" fill="#61DAFB"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="236" height="236" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
+  
+  <svg class="logo-javascript" width="150" height="236" viewBox="0 0 236 236" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+<circle cx="118" cy="114" r="111.5" stroke="#F7DF1E" stroke-width="5"/>
+<circle cx="118" cy="114" r="91" fill="#F7DF1E"/>
+<path class="path-javascript" d="M143.266 108.676C154.281 113.145 157.995 117.937 158.906 123.64L158.667 123.291C159.531 128.213 158.432 131.416 158.281 131.885C154.214 145.791 131.557 146.26 122.495 137.114C120.62 134.926 119.37 133.838 118.276 131.338L127.807 125.864C130.385 129.77 132.724 131.885 136.948 132.822C142.656 133.525 148.437 131.572 147.187 125.478C145.859 120.473 135.464 119.306 128.354 113.994C121.167 109.15 119.453 97.4315 125.391 90.7075C127.422 88.2075 130.776 86.3325 134.297 85.4732L137.969 85.0096C145.078 84.8482 149.448 86.7232 152.729 90.395C153.677 91.3273 154.374 92.2704 155.781 94.4579C152.026 96.7961 152.026 96.7963 146.641 100.317C145.464 97.8169 143.589 96.2544 141.557 95.6294C138.432 94.6919 134.448 95.7023 133.667 99.0669C133.359 100.083 133.432 101.02 133.906 102.739C135.172 105.629 139.432 106.879 143.266 108.676Z" fill="#252525"/>
+<path class="path-javascript" d="M100.411 85.9055H112.12L112.099 86.1971C112.099 96.9784 112.161 107.681 112.099 118.379C112.125 125.025 112.687 130.963 110.031 135.806C108.193 139.556 104.677 141.978 100.599 143.155C94.3333 144.562 88.349 143.702 83.8958 141.046C80.9063 139.249 78.5885 136.426 77 133.145L86.5052 127.286C86.5938 127.286 86.8385 127.76 87.1666 128.306C88.3802 130.333 89.4271 131.739 91.4896 132.76C93.5 133.39 97.9271 133.848 99.6458 130.26C100.693 128.447 100.365 122.557 100.365 116.14C100.365 106.03 100.411 95.9992 100.411 85.9055Z" fill="#252525"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="236" height="236" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
+  
+   <svg class="logo-nextjs" width="150" height="236" viewBox="0 0 236 236" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+<circle cx="118" cy="114" r="91" fill="black"/>
+<circle cx="118" cy="114" r="111.5" stroke="black" stroke-width="5"/>
+<path class="path-nextjs" d="M79.2627 68.6146V140.131C79.9539 141.472 80.475 141.825 81.5549 141.965H88.8899L90.7237 140.131V87.4106C115.685 124.279 130.056 144.664 156.739 180.244L162.698 176.118C133.538 132.93 105.37 91.4998 88.8899 66.7809H81.5549L79.2627 68.6146Z" fill="white"/>
+<path class="path-nextjs" d="M146.424 66.7809V130.045L135.88 113.542V66.7809C135.87 65.4358 136.371 64.9052 137.943 64.2595H143.673C145.408 64.8569 145.795 65.5349 146.424 66.7809Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="236" height="236" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
   
 
+
   
+   <svg class="logo-typescript" width="150" height="236" viewBox="0 0 236 236" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+<circle cx="118" cy="114" r="111.5" stroke="#007ACC" stroke-width="5"/>
+<circle cx="118" cy="114" r="90.5" fill="#007ACC" stroke="#007ACC"/>
+<path class="path-typescript" d="M161.166 74.0008C165.749 74.0008 169.808 74.268 173.349 74.8023C176.69 75.2849 179.968 76.1079 183.129 77.2573V95.0058C181.612 93.994 180 93.1213 178.314 92.3991C176.575 91.6448 174.781 91.0172 172.945 90.5217C169.468 89.5873 165.877 89.1017 162.267 89.0776C160.021 89.0776 157.976 89.2798 156.134 89.6986C154.493 90.0372 152.918 90.6271 151.469 91.446C150.196 92.1969 149.223 93.0995 148.527 94.1465C147.826 95.2018 147.462 96.4318 147.478 97.6847C147.478 99.0999 147.875 100.378 148.646 101.504C149.425 102.631 150.533 103.699 151.964 104.71C153.394 105.721 155.131 106.703 157.175 107.671C159.219 108.646 161.533 109.649 164.109 110.675C167.629 112.097 170.788 113.613 173.589 115.209C176.389 116.812 178.793 118.625 180.8 120.646C182.807 122.661 184.334 124.964 185.398 127.557C186.461 130.149 187 133.16 187 136.604C187 141.348 186.064 145.341 184.207 148.562C182.393 151.738 179.789 154.43 176.629 156.396C173.223 158.478 169.449 159.937 165.494 160.7C161.256 161.566 156.786 161.999 152.091 161.999C147.47 162.019 142.857 161.622 138.313 160.815C134.381 160.159 130.568 158.96 126.991 157.255V138.265C133.759 143.818 142.344 146.887 151.23 146.93C153.723 146.93 155.902 146.713 157.759 146.28C159.624 145.847 161.174 145.24 162.424 144.475C163.667 143.695 164.596 142.785 165.217 141.731C165.904 140.503 166.217 139.114 166.119 137.722C166.021 136.33 165.516 134.994 164.663 133.868C163.551 132.448 162.19 131.226 160.642 130.257C158.723 129.03 156.702 127.958 154.599 127.051C152.108 125.954 149.594 124.904 147.059 123.903C140.185 121.137 135.063 117.751 131.686 113.758C128.316 109.765 126.624 104.934 126.624 99.2804C126.624 94.8469 127.545 91.0416 129.387 87.8573C131.229 84.673 133.73 82.0519 136.905 79.994C140.263 77.8577 143.991 76.3194 147.913 75.4522C152.255 74.4648 156.704 73.9776 161.166 74.0008ZM48 75.3583H119.608V90.9983H93.909V160.649H73.5641V90.9983H48V75.3583Z" fill="#E8E8E8"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="236" height="236" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
 </div>
-
-  {/* <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-  <div class="bg-white rounded-lg shadow-lg p-6 transform hover:scale-105 transition duration-300">
-    <div class="text-center">
-      <i class="fas fa-code text-4xl text-blue-500"></i>
-      <h3 class="text-xl font-semibold mt-4">Web Development</h3>
-    </div>
-    <ul class="mt-4">
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        HTML/CSS
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        JavaScript
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        React
-      </li>
-    </ul>
-  </div>
-  
-  <div class="bg-white rounded-lg shadow-lg p-6 transform hover:scale-105 transition duration-300">
-    <div class="text-center">
-      <i class="fas fa-paint-brush text-4xl text-purple-500"></i>
-      <h3 class="text-xl font-semibold mt-4">Design</h3>
-    </div>
-    <ul class="mt-4">
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        UI/UX Design
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        Adobe Photoshop
-      </li>
-      <li class="flex items-center mb-2">
-        <i class="fas fa-check text-green-500 mr-2"></i>
-        Figma
-      </li>
-    </ul>
-  </div>
-  
- 
-  
-</div> */}
-
-  
 
 
     </>
